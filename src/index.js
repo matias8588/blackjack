41b6f8ef27d653daba7c7a0d69e854604d026823
Matias <matiascejas8@gmail.com>
2019-02-27 08:26:06 -0300

    se agrego la funcion de seleccionar cartas y chequeo de igualdad, falta mejorar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-import sass from './scss/main.scss'
+import './scss/main.scss'
+
+// const cartas = ['uno', 'uno1', 'dos', 'dos1', 'tres', 'tres1', 'cuatro', 'cuatro1']
+var cartasSeleccionadas = []
+
+/* Seleccion de tarjetas y chequeo de igualdad */
+window.cartaElegida = (clickedId) => {
+  console.log(clickedId)
+  cartasSeleccionadas.push(clickedId)
+  console.log(cartasSeleccionadas)
+  if (cartasSeleccionadas[0] === cartasSeleccionadas[1]) {
+    console.log('son iguales')
+    cartasSeleccionadas = []
+    document.querySelector('.carta-' + clickedId).style.display = 'none'
+    document.querySelector('.carta1-' + clickedId).style.display = 'none'
+  }
+}
+/* Fin de seleccion de tarjetas y chequeo de igualdad */
